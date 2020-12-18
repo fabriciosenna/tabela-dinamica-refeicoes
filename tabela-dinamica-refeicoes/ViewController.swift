9db4fe00bf24e505c11557dcd3543bc4ebc643a9
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-18 17:21:07 -0300

    Inserindo linhas e dados nas linhas

--- tabela-dinamica-refeicoes/ViewController.swift
+++ tabela-dinamica-refeicoes/ViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    let refeicoes = ["Churros","Macarrão","Pizza"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("tableviewcontrollerfoicarregado")
+    }
+    
+    //adicionando linhas
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    //inserindo os dados nas linhas criadas
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        let refeicao = refeicoes[indexPath.row]
+        
+        celula.textLabel?.text = refeicao
+        
+        return celula
     }
 
 
