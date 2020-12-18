1236618742f49e5066416efed27431911aed126d
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-18 16:54:24 -0300

    Início projeto demonstrando a criação de uma tabela dinämica

---
+++ tabela-dinamica-refeicoes/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  tabela-dinamica-refeicoes
+//
+//  Created by Fabricio Rodrigo Baixo on 12/18/20.
+//  Copyright © 2020 Fabricio Rodrigo Baixo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("tableviewcontrollerfoicarregado")
+    }
+
+
+}
+
